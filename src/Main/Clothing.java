78af31bad5035ac8ec9e303060f1b6e00cafc894
dimasutamaa <dimasutamapa@gmail.com>
2024-07-27 16:09:18 +0700

    first commit

---
+++ src/Main/Clothing.java
@@ -0,0 +1,5 @@
+package Main;
+
+public class Clothing extends Product implements Orderable {
+    
+}
---
+++ src/Main/Electronics.java
@@ -0,0 +1,5 @@
+package Main;
+
+public class Electronics extends Product implements Orderable {
+    
+}
